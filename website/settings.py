8f23627a21d99f386308e478e2d35f265d338633
Danny <dmoon1112@gmail.com>
2017-11-19 17:26:42 -0500

    Improved UI, Implemented responsive UI Design for different devices

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@+&g4z#v)qtg&xuqa5*93r%yd2d-$^kwgk%e!hu76&-hz+%@fm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.10']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.10', '10.10.122.83']
 
 
 # Application definition
